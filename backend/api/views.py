fca3914ed1050f55c83dbb4055a7d99e61da0b0c
Yamlak Negash <yamlaknegash96@gmail.com>
2025-09-22 06:59:21 +0300

    Registration and Checking done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView): #handles creating new user or object for us.
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  permission_class = [AllowAny] # anyone can use to create a new user.
